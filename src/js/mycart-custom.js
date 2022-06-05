5a0c7d2a4c66db13d254992ffd823fe0d10f07c0
Alice Lo Valvo <alice.lovalvo@prysmiangroup.com>
2022-06-05 18:29:59 +0200

    update cover1.jpg

--- src/js/mycart-custom.js
+++ src/js/mycart-custom.js
@@ -54,41 +54,41 @@ window.addEventListener('resize', initialHightlightLocation);
 buttons.forEach(button => button.addEventListener('click', handleClick));
 
 
-//mycart
-$(function () {
+// //mycart
+// $(function () {
 
-  var goToCartIcon = function($addTocartBtn){
-    var $cartIcon = $(".my-cart-icon");
-    var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
-    $addTocartBtn.prepend($image);
-    var position = $cartIcon.position();
-    $image.animate({
-      top: position.top,
-      left: position.left
-    }, 500 , "linear", function() {
-      $image.remove();
-    });
-  }
+//   var goToCartIcon = function($addTocartBtn){
+//     var $cartIcon = $(".my-cart-icon");
+//     var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+//     $addTocartBtn.prepend($image);
+//     var position = $cartIcon.position();
+//     $image.animate({
+//       top: position.top,
+//       left: position.left
+//     }, 500 , "linear", function() {
+//       $image.remove();
+//     });
+//   }
 
-  $('.my-cart-btn').myCart({
-    classCartIcon: 'my-cart-icon',
-    classCartBadge: 'my-cart-badge',
-    affixCartIcon: true,
-    checkoutCart: function(products) {
-      $.each(products, function(){
-        console.log(this);
-      });
-    },
-    clickOnAddToCart: function($addTocart){
-      goToCartIcon($addTocart);
-    },
-    getDiscountPrice: function(products) {
-      var total = 0;
-      $.each(products, function(){
-        total += this.quantity * this.price;
-      });
-      return total * 0.5;
-    }
-  });
+//   $('.my-cart-btn').myCart({
+//     classCartIcon: 'my-cart-icon',
+//     classCartBadge: 'my-cart-badge',
+//     affixCartIcon: true,
+//     checkoutCart: function(products) {
+//       $.each(products, function(){
+//         console.log(this);
+//       });
+//     },
+//     clickOnAddToCart: function($addTocart){
+//       goToCartIcon($addTocart);
+//     },
+//     getDiscountPrice: function(products) {
+//       var total = 0;
+//       $.each(products, function(){
+//         total += this.quantity * this.price;
+//       });
+//       return total * 0.5;
+//     }
+//   });
 
-});
+// });
